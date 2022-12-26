f0ae641338dfc851a3fbe160178f498af92d9b61
seanie5011 <seanie500@hotmail.com>
2022-12-26 21:13:04 +0000

    Support rendering of .obj files

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import pygame
 import sys
 import numpy as np
-from object_3d import Object3D
+from object_3d import Object3D, Object3D_preset
 from camera import Camera
 from projection import Projection
 
 # class to control rendering
 class SoftwareRender:
-    def __init__(self):
+    def __init__(self, model='cube', draw_vertices=True):
         # initialise pygame
         pygame.init()
+
+        # currently supported preset models / objects
+        preset_objects = [
+            'cube',
+        ]
         
         # settings
         self.WIDTH, self.HEIGHT = 1600, 900
@@ -19,20 +24,59 @@ class SoftwareRender:
         self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
         self.clock = pygame.time.Clock()
 
-        self.create_objects()
+        if model in preset_objects:  # check if model has already been hardcoded
+            self.create_preset_objects(model, draw_vertices)
+        else:
+            try:  # see if it is at a file location specified
+                vertices, faces = self.get_object_from_file(model)
+                self.create_custom_objects(vertices, faces, draw_vertices)
+            except:  # if the file checking does not work
+                    self.create_preset_objects('cube', draw_vertices)
+                    print('ERROR: model specified not found.')
 
-    def create_objects(self):
+    def create_preset_objects(self, model, draw_vertices):
         self.camera = Camera(self, [0.5, 1, -4])
         self.projection = Projection(self)
-        self.object = Object3D(self)
+        self.object = Object3D_preset(self, model, draw_vertices)
 
         # move object initially
         self.object.translate([0.2, 0.4, 0.2])
         self.object.rotate_y(np.pi / 6)
 
+    def create_custom_objects(self, vertices, faces, draw_vertices):
+        self.camera = Camera(self, [5, 5, -5])
+        self.projection = Projection(self)
+        self.object = Object3D(self, vertices, faces, draw_vertices)
+
+    def get_object_from_file(self, filename):
+        '''
+        Returns the vertices and faces specified by a .obj file
+
+        .obj files have vertices specified by lines starting with 'v ' \
+        and faces specified by lines starting with 'f '
+        '''
+
+        vertices, faces = [], []
+        with open(filename) as f:
+            # check every line
+            for line in f:
+                # if vertex
+                if line.startswith('v '):
+                    # append the coordinates in that line by using split (ignoring the 'v ')
+                    vertices.append([float(coord) for coord in line.split()[1:] + [1]])  # dont forget the 1 for homogeneous coords
+                # if face
+                elif line.startswith('f '):
+                    # each line has multiple strings of 'a/b/c' where 'a' is the index for a vertex, where each string is seperated by a space
+                    # get each seperated by a space
+                    sequence = line.split()[1:]  # ignore 'f '
+                    # append the index by getting the first value ('a'), these start from 1 so have to subtract 1 to account
+                    faces.append([int(string.split('/')[0]) - 1 for string in sequence])
+
+        return vertices, faces
+
     # control screen drawing
     def draw(self):
-        self.screen.fill(pygame.Color('darkslategray'))
+        self.screen.fill(pygame.Color('black'))
         self.object.draw()
 
     # control game loop
@@ -53,5 +97,9 @@ class SoftwareRender:
             self.clock.tick(self.FPS)  # set desired FPS
 
 if __name__ == '__main__':
-    app = SoftwareRender()
+    # settings
+    model = 'Horn.obj'
+    draw_vertices = False
+
+    app = SoftwareRender(model, draw_vertices)
     app.run()
--- object_3d.py
+++ object_3d.py
@@ -2,26 +2,26 @@ import pygame
 import numpy as np
 import transformations as tr
 
-class Object3D:
-    def __init__(self, render):
+class Object3D_preset:  # for using models that are hardcoded
+    def __init__(self, render, model, draw_vertices=True):
         self.render = render
 
-        # the vertices in homogeneous coordinates for a cube
-        # 1st tuple (index 0) is the first vertex, 2nd tuple (index 1) is the second vertex, etc
-        self.vertices = np.array([
-            (0, 0, 0, 1),
-            (0, 1, 0, 1),
-            (1, 1, 0, 1),
-            (1, 0, 0, 1),
-            (0, 0, 1, 1),
-            (0, 1, 1, 1),
-            (1, 1, 1, 1),
-            (1, 0, 1, 1),
-        ])
+        # set preset models
+        preset_vertices = {
+            'cube': np.array([
+                (0, 0, 0, 1),
+                (0, 1, 0, 1),
+                (1, 1, 0, 1),
+                (1, 0, 0, 1),
+                (0, 0, 1, 1),
+                (0, 1, 1, 1),
+                (1, 1, 1, 1),
+                (1, 0, 1, 1),
+            ]),
+        }
 
-        # the faces, connecting 4 vertices
-        # each tuple contains the indices of the 4 vertices which will be connected
-        self.faces = np.array([
+        preset_faces = {
+            'cube': np.array([
             (0, 1, 2, 3),
             (4, 5, 6, 7),
             (0, 4, 5, 1),
@@ -29,6 +29,18 @@ class Object3D:
             (1, 2, 6, 5),
             (0, 3, 7, 4),
         ])
+        }
+
+        # the vertices in homogeneous coordinates for a cube
+        # 1st tuple (index 0) is the first vertex, 2nd tuple (index 1) is the second vertex, etc
+        self.vertices = preset_vertices[model]
+
+        # the faces, connecting 4 vertices
+        # each tuple contains the indices of the 4 vertices which will be connected
+        self.faces = preset_faces[model]
+
+        # whether to draw vertices
+        self.draw_vertices = draw_vertices
 
     def draw(self):
         # get vertices from screen projections
@@ -38,12 +50,75 @@ class Object3D:
         for face in self.faces:
             polygon = vertices[face]
             if not np.any((polygon == self.render.WIDTH // 2) | (polygon == self.render.HEIGHT // 2)):  # when a vertex is 0 (not shown) in normalised clip space, it is set equal to either half-width or half-height in screen space
-                pygame.draw.polygon(self.render.screen, pygame.Color('orange'), polygon, 3)  # screen, color, vertices, width
+                pygame.draw.polygon(self.render.screen, pygame.Color('orange'), polygon, 2)  # screen, color, vertices, width
 
-        # draw vertices as points
-        for vertex in vertices:
-            if not np.any((vertex == self.render.WIDTH // 2) | (vertex == self.render.HEIGHT // 2)):  # when a vertex is 0 (not shown) in normalised clip space, it is set equal to either half-width or half-height in screen space
-                pygame.draw.circle(self.render.screen, pygame.Color('white'), vertex, 6)  # screen, color, vertices, width
+        if self.draw_vertices:
+            # draw vertices as points
+            for vertex in vertices:
+                if not np.any((vertex == self.render.WIDTH // 2) | (vertex == self.render.HEIGHT // 2)):  # when a vertex is 0 (not shown) in normalised clip space, it is set equal to either half-width or half-height in screen space
+                    pygame.draw.circle(self.render.screen, pygame.Color('white'), vertex, 4)  # screen, color, vertices, width
+
+    # projections
+    def screen_projections(self):
+        vertices = self.vertices @ self.render.camera.camera_matrix()  # world space to camera space
+        vertices = vertices @ self.render.projection.projection_matrix  # camera space to clip space
+        vertices /= vertices[:, -1].reshape(-1, 1)  # normalise clip space with respect to w, by extracting w of each and reshaping them into a compatible array before dividing (-1 means original size)
+        vertices[(vertices > 2) | (vertices < -2)] = 0  # any vertices with values greater than 1 or less than -1 are not shown, as they are out of screen (clipping) {CHANGED TO 2 FOR A JANKY FIX TO CLIPPING}
+        vertices = vertices @ self.render.projection.screen_matrix  # convert normalised clip space to screen space
+        vertices = vertices[:, :2]  # only keep (x,y) coords of each vertex in this space
+
+        return vertices
+
+    # move object
+    def translate(self, pos):
+        self.vertices = self.vertices @ tr.translate(pos)  # @ is matrix mulitplication
+
+    # scale object
+    def scale(self, s):
+        self.vertices = self.vertices @ tr.scale(s)
+
+    # rotate object in x-direction
+    def rotate_x(self, a):
+        self.vertices = self.vertices @ tr.rotate_x(a)
+
+    # rotate object in y-direction
+    def rotate_y(self, a):
+        self.vertices = self.vertices @ tr.rotate_y(a)
+
+    # rotate object in z-direction
+    def rotate_z(self, a):
+        self.vertices = self.vertices @ tr.rotate_z(a)
+
+class Object3D:  # same as above except now use custom vertices and faces
+    def __init__(self, render, vertices, faces, draw_vertices=True):
+        self.render = render
+
+        # the vertices in homogeneous coordinates for a cube
+        # 1st tuple (index 0) is the first vertex, 2nd tuple (index 1) is the second vertex, etc
+        self.vertices = vertices
+
+        # the faces, connecting 4 vertices
+        # each tuple contains the indices of the 4 vertices which will be connected
+        self.faces = faces
+
+        # whether to draw vertices
+        self.draw_vertices = draw_vertices
+
+    def draw(self):
+        # get vertices from screen projections
+        vertices = self.screen_projections()
+
+        # draw edges of each face as lines from a polygon
+        for face in self.faces:
+            polygon = vertices[face]
+            if not np.any((polygon == self.render.WIDTH // 2) | (polygon == self.render.HEIGHT // 2)):  # when a vertex is 0 (not shown) in normalised clip space, it is set equal to either half-width or half-height in screen space
+                pygame.draw.polygon(self.render.screen, pygame.Color('orange'), polygon, 2)  # screen, color, vertices, width
+
+        if self.draw_vertices:
+            # draw vertices as points
+            for vertex in vertices:
+                if not np.any((vertex == self.render.WIDTH // 2) | (vertex == self.render.HEIGHT // 2)):  # when a vertex is 0 (not shown) in normalised clip space, it is set equal to either half-width or half-height in screen space
+                    pygame.draw.circle(self.render.screen, pygame.Color('white'), vertex, 4)  # screen, color, vertices, width
 
     # projections
     def screen_projections(self):
